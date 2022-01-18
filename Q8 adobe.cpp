f29046790269befff91e134f02544777f5f70fb6
Anshika Bansal <73133734+bansalanshika@users.noreply.github.com>
2022-01-18 23:32:24 +0530

    Add files via upload

---
+++ Q8 adobe.cpp
@@ -0,0 +1,43 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string str) {
+        //Your code here
+        int size = str.size();
+        int num = 0;
+        int i = 0;
+        bool isNegative = false;
+        if(str[0] == '-'){
+            i=1;
+            isNegative = true;
+        }
+        for(;i<size;i++){
+            if(!isdigit(str[i]))
+                return -1;
+            num = num*10 + (str[i]-'0');
+        }
+        return isNegative ? (-1)*num : num;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		Solution ob;
+		cout<<ob.atoi(s)<<endl;
+	}
+}  // } Driver Code Ends
