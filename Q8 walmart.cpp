938dc24c07dc5067dd9e149aa82d1ee587c3da27
Anshika Bansal <73133734+bansalanshika@users.noreply.github.com>
2022-01-28 15:27:30 +0530

    Add files via upload

---
+++ Q8 walmart.cpp
@@ -0,0 +1,41 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+        
+       int ans=0;
+       
+       int cnt=1;
+       while(N-cnt>=0)
+       {
+           N-=cnt;
+           cnt++;
+           ans++;
+       }
+       
+    return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
